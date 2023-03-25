7a00f8a2ffa0c256f0d725b4861b82b89040e363
Thomas <thomasmckenna12gmail.com>
2023-03-25 12:37:40 -0500

    added positino: absolute; to nav links in mobile version

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 const toggleButton = document.getElementsByClassName("toggle-button")[0]
 const navBarLinks = document.getElementsByClassName("nav-bar-right")[0]
 
+
 toggleButton.addEventListener("click", () => {
     navBarLinks.classList.toggle("active")
 })
