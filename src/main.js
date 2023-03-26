60e1d740952c96dbcb67e6893500d78e27176c73
Thomas <thomasmckenna12gmail.com>
2023-03-25 19:02:01 -0500

    added some padding body css

--- src/main.js
+++ src/main.js
@@ -4,4 +4,14 @@ const navBarLinks = document.getElementsByClassName("nav-bar-right")[0]
 
 toggleButton.addEventListener("click", () => {
     navBarLinks.classList.toggle("active")
+    // toggleButton.remove()
+
+
+    const cancelTogglebttn = document.createElement("div")
+    console.log(cancelTogglebttn)
+    cancelTogglebttn.className = "cancel-button"
+    cancelTogglebttn.innerText = "Hello"
+    cancelTogglebttn.classList.toggle("active")
+    
+    navBarLinks.appendChilc(cancelTogglebttn)
 })
