e02c62f676dfd1378e0a1067f4712e062667ca0c
tom mckenna <thomasmckenna12@gmail.com>
2025-07-12 14:41:41 -0500

    init server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,21 @@
 const express = require('express')
 const app = express()
+const PORT = 3000;
+const path = require('path')
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '../src')))
 
-app.use(express.json)
+app.get('/', (req, res) => {
+    try {
+        res.sendFile(path.join(__dirname, '../src/index.html'))
+        // res.send('Hello from Thomas')
+    } catch (err) {
+        console.log(err)
+    }
+})
 
-app.get('/')
-app.post()
+
+app.listen(PORT, function(err) {
+    if (err) console.log('Error in server setup')
+    console.log(`Server listeing on Port ${PORT}`)
+}) 
