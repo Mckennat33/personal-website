d6f1f1c06e868d77a1f9d753b162fcab8d6b97b3
tom mckenna <thomasmckenna12@gmail.com>
2025-07-13 13:21:06 -0500

    bugfix: grabbed values from contact form

--- src/contact.js
+++ src/contact.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const name = document.querySelector("#name").value
-    const email = document.querySelector("#email").value
-    const message = document.querySelector("#message").value
-    const sendMessageBttn = document.querySelector('.submit-btn')
-
-    document.addEventListener('click', () => {
-        // send email to me 
-    })
-
-
-    console.log(name, email, message)
-
+    const name = document.querySelector("#name")
+    const email = document.querySelector("#email")
+    const message = document.querySelector("#message")
+    const submitMessage = document.querySelector('.submit-btn')
+    
     const contactMe = async () => {
-
+        submitMessage.addEventListener('click', (e) => { 
+            e.preventDefault()
+            const nameValue = name.value  
+            const emailValue = email.value  
+            const messageValue = message.value
+            console.log(nameValue, emailValue, messageValue)
+        })
+        
     }
+    contactMe()
     
 })
+    
+
+
+    
