e5b8948ece9d4a5f3b0950e48e894213623fb5dd
tom mckenna <thomasmckenna12@gmail.com>
2025-07-14 20:25:37 -0500

    init script for email contact

---
+++ controllers/contact.controller.js
@@ -0,0 +1,12 @@
+const express = require('express') 
+const path = require('path')
+
+
+const contact = async (req, res) => {
+    try { 
+        // need to email myself
+     } catch (err) { { console.log(err) } }
+
+
+
+}
