c539577bcdf8104424018606f3eea7f6f58af47d
tom mckenna <thomasmckenna12@gmail.com>
2025-07-14 20:38:50 -0500

    cleaned up script added export module and router

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,3 +1,7 @@
+const express = require('express')
 const router = express.Router()
+const contactController = require('../controllers/contact.controller.js')
 
 router.post('/contact', contactController.contact)
+
+module.exports = router; 
