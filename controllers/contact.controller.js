ccbd03875d8f5d6bbfbb780aa7325440c2723247
tom mckenna <thomasmckenna12@gmail.com>
2025-07-20 16:41:34 -0500

    cleaned up notes and updated email message

--- controllers/contact.controller.js
+++ controllers/contact.controller.js
@@ -2,17 +2,13 @@ const express = require('express')
 const path = require('path')
 const nodemailer = require('nodemailer') 
 
-
 const contact = async (req, res) => {
     try { 
         const {name, email, message } = req.body
-        console.log( name, email, message )
-
         const transporter = nodemailer.createTransport({
-            // host: "smtp.ethereal.email",
             host: "smtp.gmail.com",
             port: 587,
-            secure: false, // true for 465, false for other ports
+            secure: false, 
             auth: {
                 user: 'thomasmckenna12@gmail.com ',
                 pass: 'itho ueqx ciyg wffm',
@@ -21,29 +17,20 @@ const contact = async (req, res) => {
                 rejectUnauthorized: false,
               },
           });
-
           (async () => {
             try {
-
             const info = await transporter.sendMail({
                 from: `${email}`,
-                // from: `thomasmckenna12@gmail.com`,
                 to: "thomasmckenna12@gmail.com",
-                subject: `${name} is reaching out to you from your Website`,
+                subject: `${name} is reaching out to you from ${email}`,
                   //   text: `${message}`, // plain‑text body
                 html: `<b>${message}</b>`, // HTML body
             });
-            
-            // console.log("Message sent:", info.messageId);
             console.log("Message sent:", info.messageId);
-            
         } catch (err) {
             console.error('Error while sending mail', err)
         }
           })();
-
-        //   console.log(transporter)
-
      } catch (err) {
         console.log(err)  
     }
